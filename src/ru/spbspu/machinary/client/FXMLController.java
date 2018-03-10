cfb41e541f63dd40e7d90ef757f85c5f77797448
marsofandrew <marsofandrew@mail.ru>
2018-03-10 15:17:10 +0300

    add resources

--- src/ru/spbspu/machinary/client/FXMLController.java
+++ src/ru/spbspu/machinary/client/FXMLController.java
@@ -6,10 +6,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -17,22 +20,27 @@ import javafx.scene.media.MediaView;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class FXMLController implements Controller {
     @FXML
     private Label currentCommandLabel;
-    @FXML
-    private MediaView mediaView;
+
     @FXML
     private ListView<String> historyOfCommands;
 
+    @FXML
+    private GridPane pane;
+
     private ObservableList<String> commands = FXCollections.observableArrayList();
+
+    private Analyze analyze;
 
     @FXML
     public void initialize() {
         historyOfCommands.setItems(commands);
-
+        analyze = new Analyze();
     }
 
     @Override
@@ -42,13 +50,48 @@ public class FXMLController implements Controller {
             currentCommandLabel.setText(str);
             commands.add(str);
 
-            //MediaPlayer player = new MediaPlayer(media);
-            //mediaView.setMediaPlayer(player);
+            Executable show = analyze.getExec(str);
+            show.exec(pane);
         });
     }
 
-    class Analyze {
-        //private HashMap<String, MediaPlayer>
+    interface Executable {
+        void exec(GridPane pane);
+    }
+
+    static class Analyze {
+        private HashMap<String, String> mapImages;
+        private HashMap<String, String> mapVideos;
+
+        Analyze() {
+            //TODO: read data
+        }
+
+        Executable getExec(String string) {
+            Executable for_ret = pane -> {
+                Media media = new Media(new File("res/videos/1.mp4").toURI().toString());
+                MediaPlayer player = new MediaPlayer(media);
+                MediaView view = new MediaView(player);
+                view.setViewport(new Rectangle2D(0,0,640,480));
+                pane.add(view, 0, 0);
+                player.play();
+            };
+            /*
+            String imagePath = mapImages.get(string);
+            String videoPath = mapImages.get(string);
+
+            if ((imagePath != null) && (videoPath != null)) {
+                return pane -> {
+
+                };
+            } else if (imagePath != null) {
+
+            } else if (videoPath != null) {
+
+            }
+            */
+            return for_ret;
+        }
     }
 }
 
