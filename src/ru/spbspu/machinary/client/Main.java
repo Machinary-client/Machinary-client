5b4dd88e6ea4826add5fa85b3ece7619f1402c9a
marsofandrew <marsofandrew@mail.ru>
2018-03-27 12:20:57 +0300

    remove unnecessary information from main

--- src/ru/spbspu/machinary/client/Main.java
+++ src/ru/spbspu/machinary/client/Main.java
@@ -21,12 +21,7 @@ public class Main extends Application {
         } catch (FileNotFoundException | InvalidPropertiesFormatException e) {
             e.printStackTrace();
         }
-        try {
-            new TechnicalFile("processes/firstProc/default.tech");
-        } catch (IOException | InvalidTypeException e) {
-            System.err.println("WTF: "+ e.getMessage());
-            e.printStackTrace();
-        }
+
         launch(args);
         if (machineController != null) {
             machineController.finish();
